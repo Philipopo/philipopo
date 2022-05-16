b8e66d2c2ef55c19a9ed3763c41e24f625d723c9
Philipopo <philipboluwatife0@gmail.com>
2022-05-16 10:49:10 +0100

    mmm

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn project.wsgi --log-file -
+worker: YOUR_COMMAND
 web: gunicorn dep:app
 web: gunicorn wsgi:app
--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-g)(mp4ao3g7av6uv^&kz+=87b+wc&2d5^yl3rk+&y+iok4d!)9
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['*','projectcrm1.herokuapp.com']
+ALLOWED_HOSTS = ['*','https://projectcrm1.herokuapp.com/']
 
 
 
